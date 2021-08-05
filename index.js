c6783ed4508051237f5da612138e3a4327b5db4a
FranST724 <fran.tasinazzo@gmail.com>
2021-08-04 23:25:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ do {
     if (comprobacion === 'fin' || comprobacion === 'FIN' || comprobacion === 'Fin'){
         break;
     } else {
-        nombre = comprobacion;
+        var nombreLibro = comprobacion;
         var autorLibro = prompt('ingrese el autor del libro');
         var precioLibro = prompt ('ingrese el precio del libro');
-        arrayLibros.push(new Libro(autorLibro, precioLibro));
+        arrayLibros.push(new Libro(nombreLibro, autorLibro, precioLibro, cantidadLibro));
     }
 }
 while (comprobacion != 'fin' || comprobacion != 'FIN' || comprobacion != 'Fin');
@@ -36,7 +36,7 @@ console.log(arrayLibros);
 
 // Libros sin stock //
 
-var sinStock = arrayLibros.filter(libro => Libro.cantidad == 0 || Libro.stock == false);
+var sinStock = arrayLibros.filter(item => item.cantidad == 0 || item.stock == false);
 console.log (sinStock);
 document.write('<h4> Lista de Libros sin Stock (cantidad = 0 o disponible = false): </h4>')
 
@@ -75,4 +75,21 @@ for (var libro of ordenadosPrecio) {
     document.write('<ul><li>Nombre: ' + Libro.nombre + '</li>');
     document.write('<li>Autor: ' + Libro.autor + '</li>');
     document.write('<li>Precio: ' + Libro.precio + '</li></ul><br>');
+}
+
+const libros = []
+libros.push = (new Libro ('Harry Potter y la piedra filosofal', 'J. K. Rowling', '1500'));
+libros.push = (new Libro ('IT', 'Stephen King', '2700'));
+libros.push = (new Libro ('Rayuela', 'Julio Cortázar', '1500'));
+libros.push = (new Libro ('El sobrino del mago', 'C.S. Lewis', '1000'));
+libros.push = (new Libro ('El señor de los anillos: la comunidad del anillo', 'J. R. R. Tolkien', '1200'));
+libros.push = (new Libro ('Juego de tronos', 'George R. R. Martin', '2500'));
+
+for (const Libro of libros) {
+    let contenedor = document.createElement('div');
+    contenedor.innerHTML = `<h3> Nombre: ${Libro.nombre} </h3>
+                            <h4> Autor: ${Libro.autor} </h4>
+                            <h5> $ ${Libro.precio} </h5>`
+    contenedor.className = 'container'
+    document.body.appendChild(contenedor);
 }
